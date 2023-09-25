d6a43d33582d0320aa6b318082e3349d0bef7734
Eric-Mai-25 <ericmai2@gmail.com>
2023-09-25 16:02:51 -0700

    Finished phase 3 onto phase 4

--- Hook-Practice/src/components/ProductView/ProductView.js
+++ Hook-Practice/src/components/ProductView/ProductView.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import './ProductView.css'
 
 function ProductView({ products }) {
 
-  // TODO: Replace with state variable
-  const sideOpen = true;
+  const [sideOpen, setSideOpen] = useState(true)
+  const [selectedProduct, setSelectedProduct] = useState(products[0])
+  const [itemSelected, setItemSelected] = useState(true)
+
+  useEffect(()=>{
+      setSideOpen(true)
+      setItemSelected(true)
+  },[selectedProduct])
+
+  useEffect(()=>{
+    if(sideOpen === false){
+      setItemSelected(false)
+    }else {
+      setItemSelected(true)
+    }
+  }, [sideOpen])
+
+
 
   return (
     <div className="product-view">
@@ -17,7 +33,8 @@ function ProductView({ products }) {
             <ProductListItem
               key={item.id}
               product={item}
-              onClick={() => console.log('SELECT PRODUCT', item)}
+              isSelected={itemSelected && selectedProduct.id === item.id}
+              onClick={() => setSelectedProduct(item)}
             />
           )}
         </div>
@@ -25,11 +42,11 @@ function ProductView({ products }) {
       <div className="product-side-panel">
         <div className="product-side-panel-toggle-wrapper">
           <div className="product-side-panel-toggle"
-               onClick={() => console.log('TOGGLE SIDE PANEL')}>
+               onClick={() => setSideOpen(!sideOpen)}>
             {sideOpen ? '>' : '<'}
           </div>
         </div>
-        <ProductDetails visible={sideOpen} />
+        <ProductDetails product={selectedProduct} visible={sideOpen} />
       </div>
     </div>
   );
